1624b2396e0efcd7b4d4686676246cde6de58b08
Marina Camacho Varea <marinacamachovarea@outlook.com>
2020-09-13 20:02:23 +0200

    configurated map with OpenStreetMap and Leaflet.

---
+++ public/javascripts/map.js
@@ -0,0 +1,10 @@
+var map = L.map('main_map').setView([37.77493, -122.41942], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+}).addTo(map);
+// show a marker on the map
+L.marker([37.77493, -122.41942]).bindPopup('Point One').addTo(map);
+L.marker([37.77493, -122.4278]).bindPopup('Point Two').addTo(map);
