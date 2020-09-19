f038d4ddc524bfb9d4e6d37ec2d257dd44541297
Marina Camacho Varea <marinacamachovarea@outlook.com>
2020-09-19 21:42:59 +0200

    added new methods to froms

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -14,6 +14,21 @@ Bicicleta.add = function (abici) {
 	Bicicleta.allBicis.push(abici);
 };
 
+Bicicleta.findById = function (aBiciId) {
+	var aBici = Bicicleta.allBicis.find((x) => x.if == aBiciId);
+	if (aBici) return aBici;
+	else throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
+};
+
+Bicicleta.removeById = function (aBiciId) {
+	for (var i = 0; i < Bicicleta.allBicis.length; i++) {
+		if (Bicicleta.allBicis[i].id == aBiciId) {
+			Bicicleta.allBicis.splice(i, 1);
+			break;
+		}
+	}
+};
+
 var x = new Bicicleta(1, 'amarillo', 'urbanXtreme', [37.77493, -122.4134]);
 var y = new Bicicleta(2, 'rojo', 'urbanRider', [37.77493, -122.4234]);
 
--- routes/bicicletas.js
+++ routes/bicicletas.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var BicicletaController = require('../controllers/bicicleta');
 
 router.get('/', BicicletaController.bicicleta_list);
-
+router.get('/create', BicicletaController.bicicleta_create_get);
+router.post('/create', BicicletaController.bicicleta_create_post);
 module.exports = router;
